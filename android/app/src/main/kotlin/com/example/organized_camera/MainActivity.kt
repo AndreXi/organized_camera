1e740aa68285e4431815695c93cd994e7c845b94
AndreXi <andrexilol@gmail.com>
2023-02-11 13:46:10 -0400

    first commit

---
+++ android/app/src/main/kotlin/com/example/organized_camera/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.organized_camera
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/sample_feature/sample_item_details_view.dart
@@ -0,0 +1,44 @@
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
+
+/// Displays detailed information about a SampleItem.
+class SampleItemDetailsView extends StatefulWidget {
+  const SampleItemDetailsView({super.key});
+
+  static const routeName = '/sample_item';
+
+  @override
+  State<SampleItemDetailsView> createState() => _SampleItemDetailsViewState();
+}
+
+class _SampleItemDetailsViewState extends State<SampleItemDetailsView> {
+  String? selectedDirectory;
+
+  @override
+  Widget build(BuildContext context) {
+    openTest() async {
+      FilePicker.platform.getDirectoryPath().then((value) => setState(() async {
+            selectedDirectory = value;
+          }));
+
+      if (selectedDirectory == null) {
+        // User canceled the pickera
+      }
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Item Details'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const Text('More Information Here'),
+            TextButton(onPressed: openTest, child: const Text("Test")),
+            Text(selectedDirectory ?? "None")
+          ],
+        ),
+      ),
+    );
+  }
+}

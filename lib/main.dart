2d8a0964bc0e3d923d08b0116ac563a168e1f579
AndreXi <andrexilol@gmail.com>
2023-03-04 13:17:28 -0400

    fix: remove unused things

--- lib/main.dart
+++ lib/main.dart
@@ -5,18 +5,8 @@ import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:organized_camera/src/models/saved_directory/saved_directory.dart';
 import 'src/app.dart';
-import 'src/settings/settings_controller.dart';
-import 'src/settings/settings_service.dart';
 
 void main() async {
-  // Set up the SettingsController, which will glue user settings to multiple
-  // Flutter Widgets.
-  final settingsController = SettingsController(SettingsService());
-
-  // Load the user's preferred theme while the splash screen is displayed.
-  // This prevents a sudden theme change when the app is first displayed.
-  await settingsController.loadSettings();
-
   // Initialize Hive database
   await Hive.initFlutter();
 
@@ -154,5 +144,5 @@ void main() async {
   // Run the app and pass in the SettingsController. The app listens to the
   // SettingsController for changes, then passes it further down to the
   // SettingsView.
-  runApp(MyApp(settingsController: settingsController));
+  runApp(const MyApp());
 }
